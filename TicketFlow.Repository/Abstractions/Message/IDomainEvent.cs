c7f9afa2ca595f93f94a222318d5ee2ee3b04123
Nhuan <nhuan.study.net@gmail.com>
2026-07-30 00:15:19 +0700

    thêm domain event

---
+++ TicketFlow.Repository/Abstractions/Message/IDomainEvent.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace TicketFlow.Repository.Abstractions.Message;
+
+public interface IDomainEvent: INotification
+{
+    public Guid Id { get; init; }
+}
---
+++ TicketFlow.Repository/Abstractions/Message/IDomainEventHandler.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace TicketFlow.Repository.Abstractions.Message;
+
+public interface IDomainEventHandler<TEvent>: INotificationHandler<TEvent> where TEvent: IDomainEvent { }
